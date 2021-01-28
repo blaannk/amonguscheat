4a50421514e3b113f18b4f94f441f8700b53bef5
blank <69248059+blaannk@users.noreply.github.com>
2021-01-28 20:12:54 +0100

    Update amongus.py

--- amongus.py
+++ amongus.py
@@ -18,3 +18,19 @@ def speedhack(speedvalue):
         pm.write_float(speed_ptr, float(speedvalue))
     except Exception as e:
         print("SpeedHack Error :", str(e))
+
+def impostor():
+    A_ptr = module + 0x01C57F7C
+    A = pm.read_int(A_ptr)
+    B_ptr = A + 0x5C
+    B = pm.read_int(B_ptr)
+    C_ptr = B + 0x0
+    C = pm.read_int(C_ptr)
+    D_ptr = C + 0x34
+    D = pm.read_int(D_ptr)
+    forceimpostor_ptr = D + 0x28
+    isImpostor = pm.read_int(forceimpostor_ptr)
+    if isImpostor == 0:
+        pm.write_int(forceimpostor_ptr, 1)
+    elif isImpostor == 1:
+        pm.write_int(forceimpostor_ptr, 0)
