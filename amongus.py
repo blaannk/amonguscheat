5d7b89f0c3eaeb91454d993cfac38cc5abe2d1f5
blank <69248059+blaannk@users.noreply.github.com>
2021-01-30 18:48:00 +0100

    Update amongus.py

--- amongus.py
+++ amongus.py
@@ -1,4 +1,4 @@
-import pymem, pymem.process
+import pymem, pymem.process, time
 
 pm = pymem.Pymem("Among Us.exe")
 module = pymem.process.module_from_name(pm.process_handle, "GameAssembly.dll").lpBaseOfDll
@@ -34,3 +34,31 @@ def impostor():
         pm.write_int(forceimpostor_ptr, 1)
     elif isImpostor == 1:
         pm.write_int(forceimpostor_ptr, 0)
+
+def killcooldown(valueasked):
+    A_ptr = module + 0x01C57F7C
+    A = pm.read_int(A_ptr)
+    B_ptr = A + 0x5C
+    B = pm.read_int(B_ptr)
+    C_ptr = B + 0x4
+    C = pm.read_int(C_ptr)
+    resetkill_ptr = C + 0x20
+    cooldown = pm.read_float(resetkill_ptr)
+    pm.write_float(resetkill_ptr, float(valueasked))
+
+def resetkill():
+    A_ptr = module + 0x01C57F7C
+    A = pm.read_int(A_ptr)
+    B_ptr = A + 0x5C
+    B = pm.read_int(B_ptr)
+    C_ptr = B + 0x0
+    C = pm.read_int(C_ptr)
+    resetkill_ptr = C + 0x44
+    while True:
+        try:
+            cooldown = pm.read_float(resetkill_ptr)
+            if cooldown > 0:
+                pm.write_float(resetkill_ptr, float(0))
+            time.sleep(0.5)
+        except:
+            pass
